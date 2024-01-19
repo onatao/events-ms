9c65bc806ab44ac5f328def1a18fc17ae79aa82d
Nathan <nathanbarrosnt@gmail.com>
2024-01-19 18:50:50 -0300

    release: structure

---
+++ src/main/java/com/neidev/eventsms/EventsMsApplication.java
@@ -0,0 +1,13 @@
+package com.neidev.eventsms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventsMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventsMsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/neidev/eventsms/EventsMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neidev.eventsms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventsMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
